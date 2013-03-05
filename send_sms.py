d05f7e26946ebf45a2182526b8ae55e1068ec4f2
ivyshushu <ivyshushu@gmail.com>
2013-03-04 19:37:36 -0800

    Update send_sms.py
    delete auth

--- send_sms.py
+++ send_sms.py
@@ -1,9 +1,9 @@
 from twilio.rest import TwilioRestClient
  
 # Find these values at https://twilio.com/user/account
-account_sid = 'AC25ef232740072dc660a9b676d9e5c992'
-auth_token = 'ff5269d59167dedf051268d5ef3e4b91'
+account_sid = ''
+auth_token = ''
 client = TwilioRestClient(account_sid, auth_token)
  
-message = client.sms.messages.create(to="+16314134223", from_="+13473086943",
+message = client.sms.messages.create(to="", from_="+13473086943",
                                      body="Hello there!")
